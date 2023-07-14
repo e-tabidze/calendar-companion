a4b944fcb6c7af66ca3de4153b4841b15d225a64
Omar Shiriniani <o.shiriniani@tnet.ge>
2023-07-14 17:20:31 +0400

    test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,83 +1,84 @@
 // ** Interfaces
-import { TailwindDiv } from 'src/interfaces/tailwind'
+// import { TailwindDiv } from 'src/interfaces/tailwind'
 import DefaultLayout from 'src/layouts/DefaultLayout'
-import { LargerContainer, SmallerContainer, MaxWidthContainer, ResponsiveContainer } from 'src/styled/styles'
-import Carousel from 'src/views/components/carousel'
-import CategoryCard from 'src/views/components/categoryCard'
-import Divider from 'src/views/components/divider'
-import ProductCard from 'src/views/components/productCard'
-import Typography from 'src/views/components/typography'
-import ActionCard from 'src/views/pages/main/actionCard'
-import Cities from 'src/views/pages/main/cities'
-import Hero from 'src/views/pages/main/hero'
+
+// import { LargerContainer, SmallerContainer, MaxWidthContainer, ResponsiveContainer } from 'src/styled/styles'
+// import Carousel from 'src/views/components/carousel'
+// import CategoryCard from 'src/views/components/categoryCard'
+// import Divider from 'src/views/components/divider'
+// import ProductCard from 'src/views/components/productCard'
+// import Typography from 'src/views/components/typography'
+// import ActionCard from 'src/views/pages/main/actionCard'
+// import Cities from 'src/views/pages/main/cities'
+// import Hero from 'src/views/pages/main/hero'
 
 // ** Tailwind Styled
-import tw from 'tailwind-styled-components'
+// import tw from 'tailwind-styled-components'
 
 // ** Styled Components
-const MainPageBox = tw.div<TailwindDiv>`flex w-full items-center flex-col`
-const ActionCardsWrapper = tw.div<TailwindDiv>`flex gap-6 flex-col -mt-16 tablet:flex-row tablet:mt-16`
+// const MainPageBox = tw.div<TailwindDiv>`flex w-full items-center flex-col`
+// const ActionCardsWrapper = tw.div<TailwindDiv>`flex gap-6 flex-col -mt-16 tablet:flex-row tablet:mt-16`
 
-const productArray = [
-  <ProductCard key={1} />,
-  <ProductCard key={2} />,
-  <ProductCard key={3} />,
-  <ProductCard key={4} />,
-  <ProductCard key={5} />,
-  <ProductCard key={6} />,
-  <ProductCard key={7} />,
-  <ProductCard key={8} />,
-  <ProductCard key={9} />,
-  <ProductCard key={10} />
-]
+// const productArray = [
+//   <ProductCard key={1} />,
+//   <ProductCard key={2} />,
+//   <ProductCard key={3} />,
+//   <ProductCard key={4} />,
+//   <ProductCard key={5} />,
+//   <ProductCard key={6} />,
+//   <ProductCard key={7} />,
+//   <ProductCard key={8} />,
+//   <ProductCard key={9} />,
+//   <ProductCard key={10} />
+// ]
 
-const categoryArray = [
-  {
-    id: 1,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 2,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 3,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 4,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 5,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 6,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 7,
-    type: 'ეკონომიური',
-    available: 231
-  },
-  {
-    id: 8,
-    type: 'ეკონომიური',
-    available: 231
-  }
-]
+// const categoryArray = [
+//   {
+//     id: 1,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 2,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 3,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 4,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 5,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 6,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 7,
+//     type: 'ეკონომიური',
+//     available: 231
+//   },
+//   {
+//     id: 8,
+//     type: 'ეკონომიური',
+//     available: 231
+//   }
+// ]
 
-const categories = categoryArray.map((item, key) => <CategoryCard category={item.type} key={key} />)
+// const categories = categoryArray.map((item, key) => <CategoryCard category={item.type} key={key} />)
 const MainPage = () => (
   <DefaultLayout>
-    <MainPageBox>
+    {/* <MainPageBox>
       <LargerContainer>
         <Hero />
       </LargerContainer>
@@ -132,7 +133,7 @@ const MainPage = () => (
           />
         </ActionCardsWrapper>
       </SmallerContainer>
-    </MainPageBox>
+    </MainPageBox> */}
   </DefaultLayout>
 )
 
