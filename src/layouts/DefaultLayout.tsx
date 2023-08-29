0bee248993af41f0cab84bf83a995f059fddca75
mariakvinikadze <mariakvinikadze90@gmail.com>
2023-08-30 02:04:37 +0400

    search page map

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,13 +3,14 @@ import { HeaderContainer } from 'src/styled/styles'
 import Header from 'src/views/components/header'
 
 interface Props {
-  children?: any
+  children?: any,
+  fullWidth?:boolean
 }
 
-const DefaultLayout = ({ children }: Props) => {
+const DefaultLayout = ({ children, fullWidth }: Props) => {
   return (
     <main>
-      <HeaderContainer style={{position: 'sticky'}}>
+      <HeaderContainer style={{position: 'sticky'}} className={`${fullWidth?'w-full px-10':'max-w-[1470px] px-2 laptop:px-8 2xl:px-0'}`}>
         <Header />
       </HeaderContainer>
       <div>{children}</div>
--- src/views/pages/search/styles.ts
+++ src/views/pages/search/styles.ts
@@ -1,11 +1,11 @@
 import tw from 'tailwind-styled-components'
 import { TailwindComponent } from 'src/interfaces/tailwind'
 
-export const FiltersWrapper = tw.div<TailwindComponent>`hidden large:flex justify-between items-center mt-8`
+export const FiltersWrapper = tw.div<TailwindComponent>`hidden laptop:flex justify-between items-center mt-8`
 export const MainFilters = tw.div<TailwindComponent>`flex gap-4 w-max`
 export const ClearFiltersWrapper = tw.div<TailwindComponent>`flex items-center gap-4`
-export const ResponsiveDivider = tw.div`hidden large:block w-full my-8 h-px bg-raisin-10`
-export const SearchContentsContainer = tw.div<TailwindComponent>`flex gap-8`
+export const ResponsiveDivider = tw.div`hidden large:block w-full mt-8 h-px bg-raisin-10`
+export const SearchContentsContainer = tw.div<TailwindComponent>`flex flex-col`
 export const SearchResultsContainer = tw.div<TailwindComponent>`flex justify-between flex-col-reverse items-baseline large:flex-row large:items-center`
-export const MapContainer = tw.div<TailwindComponent>`w-full fixed top-36 left-0 large:w-1/2 large:relative large:top-0`
+export const MapContainer = tw.div<TailwindComponent>`flex`
 export const Wrapper = tw.div<TailwindComponent>``
