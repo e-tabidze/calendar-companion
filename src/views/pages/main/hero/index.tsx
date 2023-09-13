a8195f62f1b9d5ea6bf1ff3e08981d4d7879e13e
mariakvinikadze <mariakvinikadze90@gmail.com>
2023-09-13 15:53:17 +0400

    hero fix

--- src/views/pages/main/hero/index.tsx
+++ src/views/pages/main/hero/index.tsx
@@ -6,18 +6,20 @@ const Hero = () => {
   return (
     <HeroContainer>
       <Copy>
-        <Typography
-          type='h1'
-          className='mt-[120px] md:[mt-0] text-white font-bold  text-[20px] md:text-[40px] lg:text-[48px] xl:text-[54px] lg:leading-[64px] xl:leading-[78px]'
-        >
-          მიიღეთ მეტი, ვიდრე ავტომობილია
-        </Typography>
-        <Typography type='h3' className='text-white text-[16px] mt-3 hidden md:inline-block'>
-          შეარჩიეთ სასურველი ავტომობილი ონლაინ და გაიმარტივეთ ქირაობის პროცესი. ისარგებლეთ დამატებითი სერვისებით და
-          შეიქმენით მაქსიმალური კომფორტი.
-        </Typography>
+        <div className="lg:w-11/12">
+          <Typography
+              type='h1'
+              className='mt-[120px] md:mt-0 text-white font-bold text-[20px] md:text-[40px] lg:text-[42px] xl:text-[54px] lg:leading-[56px] xl:leading-[78px]'
+          >
+            მიიღეთ მეტი, ვიდრე ავტომობილია
+          </Typography>
+          <Typography type='h3' className='text-white text-[16px] mt-3 hidden md:inline-block'>
+            შეარჩიეთ სასურველი ავტომობილი ონლაინ და გაიმარტივეთ ქირაობის პროცესი. ისარგებლეთ დამატებითი სერვისებით და
+            შეიქმენით მაქსიმალური კომფორტი.
+          </Typography>
+        </div>
+        <Filters />
       </Copy>
-      <Filters />
     </HeroContainer>
   )
 }
--- src/views/pages/main/hero/styles.ts
+++ src/views/pages/main/hero/styles.ts
@@ -1,5 +1,5 @@
 import tw from 'tailwind-styled-components'
 import { TailwindComponent } from 'src/interfaces/tailwind'
 
-export const HeroContainer = tw.div<TailwindComponent>`bg-[url("/images/banner.png")] bg-no-repeat bg-center bg-cover rounded-2xl m-auto p-3 md:p-0 md:py-[40px] lg:py-[84px] md:px-[40px] lg:px-[80px] xl:px-[112px]`
-export const Copy = tw.div<TailwindComponent>`w-full lg:w-9/12 xl:w-7/12 h-full flex flex-col justify-between`
+export const HeroContainer = tw.div<TailwindComponent>`bg-[url("/images/banner.png")] bg-no-repeat bg-center bg-cover xl:h-[560px] rounded-2xl m-auto p-3 md:p-0 md:py-[40px] lg:py-[40px] xl:py-[84px] md:px-[40px] lg:px-[40px] xl:px-[112px]`
+export const Copy = tw.div<TailwindComponent>`w-full lg:w-[800px] h-full flex flex-col justify-between`
