68eabd996a2557c222e57ae3493836ab5744b34d
Elene Tabidze <e.tabidze@myauto.ge>
2023-10-23 16:19:47 +0400

    refetch company services after adding new service & new service validation

--- src/views/pages/dashboard/newProduct/stepFour/newServiceModal/index.tsx
+++ src/views/pages/dashboard/newProduct/stepFour/newServiceModal/index.tsx
@@ -16,7 +16,7 @@ interface Props {
 }
 
 const NewServiceModal: React.FC<Props> = ({ open, onClose }) => {
-  const { control, handleSubmit, createNewService, serviceValues } = useNewService()
+  const { control, handleSubmit, createNewService, serviceValues, errors } = useNewService()
 
   const options = [
     {
@@ -33,15 +33,6 @@ const NewServiceModal: React.FC<Props> = ({ open, onClose }) => {
     }
   ]
 
-  // const onSubmit = async () => {
-  //   try {
-  //     console.log(serviceValues, 'serviceValues')
-  //     await createNewService(serviceValues, Cookie.get('AccessToken'))
-  //   } catch (error) {
-  //     console.error('An error occurred while creating new listing:', error)
-  //   }
-  // }
-
   const queryClient = useQueryClient()
 
   const createNewServiceMutation = useMutation(
@@ -50,9 +41,8 @@ const NewServiceModal: React.FC<Props> = ({ open, onClose }) => {
     },
     {
       onSuccess: () => {
-        // Invalidate and refetch the service list query after a successful mutation
-        queryClient.invalidateQueries(['serviceList']) // Adjust 'serviceList' to your actual query key
-        onClose() // Close the modal
+        queryClient.invalidateQueries(['companyServices'])
+        onClose()
       }
     }
   )
@@ -90,18 +80,16 @@ const NewServiceModal: React.FC<Props> = ({ open, onClose }) => {
               <Dialog.Panel className='w-full max-w-[800px] transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all'>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <Dialog.Title as='h3' className='w-full flex items-center justify-between px-10 py-6'>
-                    <Typography type='h5' weight='normal' className='text-2md'>
-                      ახალი სერვისის დამატება
-                    </Typography>
+                    ახალი სერვისის დამატება
                     <IconButton icon='/icons/close.svg' onClick={onClose} width={40} height={40} />
                   </Dialog.Title>
                   <Divider />
                   <div className='p-6 mb-20'>
                     <div className='flex flex-col gap-4'>
-                      <DefaultInput label='სერვისის დასახელება' control={control} name='title' />
-                      <DefaultInput label='აღწერა' control={control} name='description' rows={4} />
+                      <DefaultInput label='სერვისის დასახელება' control={control} name='title' errors={errors} />
+                      <DefaultInput label='აღწერა' control={control} name='description' rows={4} errors={errors} />
                     </div>
-                    <SelectField options={options} control={control} name='type_id' />
+                    <SelectField options={options} control={control} name='type_id' errors={errors} />
                   </div>
                   <div className='flex justify-end absolute bottom-0 w-full shadow-md'>
                     <DefaultButton
