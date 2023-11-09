61085b0da49560d851efcefb03154c3a85531a97
Elene Tabidze <e.tabidze@myauto.ge>
2023-11-09 20:40:29 +0400

    checkbox

--- src/views/pages/dashboard/newProduct/stepTwo/index.tsx
+++ src/views/pages/dashboard/newProduct/stepTwo/index.tsx
@@ -138,7 +138,7 @@ const StepTwo: React.FC<Props> = ({ control, appendAdditionalParam, step }) => {
           <>Loading</>
         ) : (
           <>
-            {additionalParams?.map((param: { id: Key | null | undefined; title: string }, index: any) => (
+            {/* {additionalParams?.map((param: { id: Key | null | undefined; title: string }, index: any) => (
               <div className='my-2' key={param.id}>
                 <CheckboxField
                   name={`additional_options.${index}`}
@@ -147,7 +147,15 @@ const StepTwo: React.FC<Props> = ({ control, appendAdditionalParam, step }) => {
                   append={() => appendAdditionalParam({ id: param.id, title: param.title })}
                 />
               </div>
-            ))}
+            ))} */}
+            <div className='my-2'>
+              <CheckboxField
+                name={`additional_options`}
+                control={control}
+                options={additionalParams}
+                append={() => appendAdditionalParam(appendAdditionalParam)}
+              />
+            </div>
           </>
         )}
       </div>
--- src/views/pages/search/categoryPopover/index.tsx
+++ src/views/pages/search/categoryPopover/index.tsx
@@ -1,6 +1,8 @@
 import PopoverDropdown from 'src/views/components/popoverDropdown'
 import Checkbox from 'src/views/components/checkboxField'
 import useFilters from 'src/hooks/useFilters'
+import { useWatch } from 'react-hook-form'
+import CheckboxField from 'src/views/components/checkboxField'
 
 interface Props {
   control: any
@@ -11,9 +13,13 @@ interface Props {
 const CategoryPopover: React.FC<Props> = ({ control, appendCategory, removeCategory }) => {
   const { categoriesFilter } = useFilters()
 
+  const formState = useWatch({ control })
+
+  console.log(formState.category, 'formState')
+
   return (
     <PopoverDropdown label='კატეგორია' maxWidth='max-w-md'>
-      {categoriesFilter?.map((category: any, index: number) => (
+      {/* {categoriesFilter?.map((category: any, index: number) => (
         <div key={category.id} className='my-2'>
           <Checkbox
             title={category.title}
@@ -21,11 +27,20 @@ const CategoryPopover: React.FC<Props> = ({ control, appendCategory, removeCateg
             name={`category.${index}`}
             control={control}
             append={() => appendCategory(category.id)}
-            customValue={category.id} 
             remove={() => removeCategory(index)}
+            customValue={category.index}
           />
         </div>
-      ))}
+      ))} */}
+
+      {/* <Checkbox
+            label={category.title}
+            iconPath={category.icon}
+            name={`category.${index}`}
+            control={control}
+            append={() => appendCategory(category.id)}
+          /> */}
+      <CheckboxField options={categoriesFilter} name='category' control={control} append={appendCategory} />
     </PopoverDropdown>
   )
 }
