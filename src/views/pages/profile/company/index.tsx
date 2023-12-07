f34be02279384946848233525f42ff628de74fc4
Elene Tabidze <e.tabidze@myauto.ge>
2023-12-07 22:38:00 +0400

    fix bug deleting company

--- src/views/pages/profile/company/index.tsx
+++ src/views/pages/profile/company/index.tsx
@@ -85,7 +85,7 @@ const Company: React.FC<Props> = ({ id, name, productsCount, logo }) => {
         <div className='p-2 md:p-6'>
           <div className='flex items-center gap-6 md:mb-10'>
             <div className='flex items-center justify-center border border-raisin-10 rounded-2xl md:rounded-3xl w-[76px] h-[76px] md:w-24 md:h-24'>
-              <Image src={logo} alt='' height={96} width={97} className='rounded-3xl' />
+              <Image src={logo || ''} alt='' height={96} width={97} className='rounded-3xl' />
             </div>
             <div>
               <Typography type='h3' className='font-bold text-3md md:text-2lg'>
