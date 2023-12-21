594dc58697d7c76ec8350b0498c91c8c56fbfcda
Tornike Kurdgelashvili <t.kurdgelashvili@my.ge>
2023-12-21 15:05:52 +0400

    new-build

--- src/views/components/selectField/index.tsx
+++ src/views/components/selectField/index.tsx
@@ -140,6 +140,7 @@ const SelectField: React.FC<Props> = ({
               isClearable
               placeholder={placeholder}
               isDisabled={disabled}
+              
               // @ts-ignore
               emoji={
                 icon && (
