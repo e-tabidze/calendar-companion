b4e69ff4717fec24fb745e4e26ee05e6874c726b
Tornike Kurdgelashvili <t.kurdgelashvili@my.ge>
2023-12-22 00:33:03 +0400

    changes

--- src/views/pages/main/hero/styles.ts
+++ src/views/pages/main/hero/styles.ts
@@ -1,5 +1,5 @@
 import tw from 'tailwind-styled-components'
 import { TailwindComponent } from 'src/interfaces/tailwind'
 
-export const HeroContainer = tw.div<TailwindComponent>`relative overflow-hidden xl:h-[560px] rounded-2xl m-auto p-3 md:p-0 md:py-10 lg:py-10 xl:py-21 md:px-10 lg:px-10 xl:px-[112px]`
+export const HeroContainer = tw.div<TailwindComponent>`relative xl:h-[560px] m-auto p-3 md:p-0 md:py-10 lg:py-10 xl:py-21 md:px-10 lg:px-10 xl:px-[112px]`
 export const Copy = tw.div<TailwindComponent>`w-full lg:w-[800px] h-full flex flex-col justify-between`
