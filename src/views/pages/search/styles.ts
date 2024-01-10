5037bdc171f121f9800d03bd210901d45567d3c1
Tornike Kurdgelashvili <t.kurdgelashvili@my.ge>
2024-01-11 01:01:09 +0400

    fixes

--- src/views/pages/search/styles.ts
+++ src/views/pages/search/styles.ts
@@ -6,6 +6,6 @@ export const MainFilters = tw.div<TailwindComponent>`flex gap-4 w-max`
 export const ClearFiltersWrapper = tw.div<TailwindComponent>`flex items-center gap-4`
 export const ResponsiveDivider = tw.div`hidden lg:block w-full h-px bg-raisin-10`
 export const SearchContentsContainer = tw.div<TailwindComponent>`flex flex-col`
-export const SearchResultsContainer = tw.div<TailwindComponent>`flex justify-between flex-col-reverse items-baseline md:flex-row md:items-center pt-6 pb-4`
+export const SearchResultsContainer = tw.div<TailwindComponent>`flex justify-between flex-col-reverse items-baseline md:flex-row md:items-center pt-6 md:pb-4 pb-5`
 export const MapContainer = tw.div<TailwindComponent>`flex`
 export const Wrapper = tw.div<TailwindComponent>``
