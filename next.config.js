b2abaf4fb188d1d726ce3c1c3d38eeaced38b357
Elene Tabidze <e.tabidze@myauto.ge>
2024-01-12 13:24:25 +0400

    user scale

--- next.config.js
+++ next.config.js
@@ -9,6 +9,15 @@ module.exports = {
     domains: ['static.my.ge', 'test.static.my.ge']
   },
 
+  head: {
+    meta: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+      },
+    ],
+  },
+
   i18n,
 
   webpack: config => {
