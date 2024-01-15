ba2dfae07e7c36d97646e804b243ff80ba9c7cd0
Elene Tabidze <e.tabidze@myauto.ge>
2024-01-15 15:14:35 +0400

    fix notifications

--- src/views/components/defaultHeader/notification/index.tsx
+++ src/views/components/defaultHeader/notification/index.tsx
@@ -42,56 +42,60 @@ const Notification = () => {
             <Menu.Items className='w-[348px] absolute z-2 top-full left-1/2 -translate-x-1/2 mt-5 bg-white rounded-2xl shadow-sm'>
               <div className='border-b-1 border-raisin-10 p-4 text-md font-bold text-raisin-100'>შეტყობინებები</div>
               <ul className='py-6 px-3 max-h-[320px] overflow-y-auto'>
-                {notifictions
-                  ?.filter((not: any) => not.read_at === null)
-                  ?.map((notification: any) => (
-                    <li key={notification?.id}>
-                      <a href='#' className='px-4 flex items-center py-2 relative'>
-                        <span className='w-1 h-1 rounded-full bg-orange-100 hover:bg-orange-110 transition-all absolute left-[6px] top-1/2 -translate-y-1/2'></span>
-                        <span className='w-10 h-10 bg-grey-100 rounded-xl mr-4 flex items-center justify-center shrink-0'>
-                          <Icon
-                            svgPath={`${
-                              notification?.data.color === 'yellow'
-                                ? 'notification'
-                                : notification?.data.color === 'red'
-                                ? 'info'
-                                : 'notification'
-                            }`}
-                            width={24}
-                            height={24}
-                            className={`${
-                              notification?.data.color === 'yellow'
-                                ? 'fill-yellow-110'
-                                : notification?.data.color === 'red'
-                                ? 'fil-orange-110'
-                                : 'fill-green-100'
-                            }`}
-                          />
-                        </span>
-                        <div className='flex flex-col'>
-                          <Typography type='h5' className='text-2sm font-medium text-raisin-100'>
-                            <Link
-                              href={`/${!!activeCompany ? '/dashboard' : '/profile'}/notifications/?id=${
-                                notification?.id
-                              }&company=${notification?.data?.company_id}`}
-                            >
-                              {notification?.data?.text}
-                            </Link>
-                          </Typography>
-                          <Typography type='subtitle' className='text-sm font-normal text-raisin-30'>
-                            {format(parseISO(notification?.created_at), 'd MMM yyyy', { locale: ka })}
-                          </Typography>
-                        </div>
-                      </a>
-                    </li>
-                  ))}
+                {notifictions?.length > 0 ? (
+                  notifictions
+                    ?.filter((not: any) => not.read_at === null)
+                    ?.map((notification: any) => (
+                      <li key={notification?.id}>
+                        <a href='#' className='px-4 flex items-center py-2 relative'>
+                          <span className='w-1 h-1 rounded-full bg-orange-100 hover:bg-orange-110 transition-all absolute left-[6px] top-1/2 -translate-y-1/2'></span>
+                          <span className='w-10 h-10 bg-grey-100 rounded-xl mr-4 flex items-center justify-center shrink-0'>
+                            <Icon
+                              svgPath={`${
+                                notification?.data.color === 'yellow'
+                                  ? 'notification'
+                                  : notification?.data.color === 'red'
+                                  ? 'info'
+                                  : 'notification'
+                              }`}
+                              width={24}
+                              height={24}
+                              className={`${
+                                notification?.data.color === 'yellow'
+                                  ? 'fill-yellow-110'
+                                  : notification?.data.color === 'red'
+                                  ? 'fil-orange-110'
+                                  : 'fill-green-100'
+                              }`}
+                            />
+                          </span>
+                          <div className='flex flex-col'>
+                            <Typography type='h5' className='text-2sm font-medium text-raisin-100'>
+                              <Link
+                                href={`/${!!activeCompany ? '/dashboard' : '/profile'}/notifications/?id=${
+                                  notification?.id
+                                }&company=${notification?.data?.company_id}`}
+                              >
+                                {notification?.data?.text}
+                              </Link>
+                            </Typography>
+                            <Typography type='subtitle' className='text-sm font-normal text-raisin-30'>
+                              {format(parseISO(notification?.created_at), 'd MMM yyyy', { locale: ka })}
+                            </Typography>
+                          </div>
+                        </a>
+                      </li>
+                    ))
+                ) : (
+                  <Typography type='subtitle' className='text-center'>შეტყობინებები ჯერ არ გაქვს</Typography>
+                )}
               </ul>
-              <Link
+             {notifictions?.length > 0 && <Link
                 className='flex justify-center border-t-1 border-raisin-10 p-4 text-2sm font-medium text-raisin-100'
                 href={!!activeCompany ? '/dashboard/notifications' : '/profile/notifications'}
               >
                 ნახე ყველა
-              </Link>
+              </Link>}
             </Menu.Items>
           </Transition>
         </>
