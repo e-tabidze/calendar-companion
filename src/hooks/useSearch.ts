26559ae872534ba9af99cb032c82829687655a7e
Elene Tabidze <e.tabidze@myauto.ge>
2024-03-20 19:16:02 +0400

    moved ubjtouri to utils

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import { useFieldArray, useForm, useWatch } from 'react-hook-form'
 import { queryClient } from 'src/pages/_app'
 import SearchService from 'src/services/SearchService'
+import { objectToURI } from 'src/utils/objectToURI'
 
 const useSearch = () => {
   const urlSearchParams = typeof window !== 'undefined' ? new URLSearchParams(window?.location.search) : null
@@ -172,26 +173,6 @@ const useSearch = () => {
       console.error('Error creating product:', error)
       throw error
     }
-  }
-
-  const objectToURI = (obj: any) => {
-    return Object.entries(obj)
-      .filter(([value]) => {
-        return value !== null && value !== undefined && !(Array.isArray(value) && value.length === 0)
-      })
-      .map(([key, value]: [string, any]) => {
-        if (key === 'booking' && value !== null && value !== undefined) {
-          const { book_from, book_to } = value
-
-          return [`book_from=${encodeURIComponent(book_from)}`, `book_to=${encodeURIComponent(book_to)}`]
-        } else if (Array.isArray(value)) {
-          return value.map(v => `${encodeURIComponent(key)}[]=${encodeURIComponent(v)}`)
-        } else {
-          return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
-        }
-      })
-      .flat()
-      .join('&')
   }
 
   return {
