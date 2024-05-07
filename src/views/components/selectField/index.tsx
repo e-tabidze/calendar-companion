53ff331968196cfb7f5ff3333ae6260d940495af
Elene Tabidze <e.tabidze@myauto.ge>
2024-05-07 11:33:00 +0400

    build

--- src/views/components/selectField/index.tsx
+++ src/views/components/selectField/index.tsx
@@ -180,7 +180,7 @@ const SelectField: React.FC<Props> = ({
               isClearable
               placeholder={placeholder}
               isDisabled={disabled}
-              
+                
               // @ts-ignore
               emoji={
                 icon && (
