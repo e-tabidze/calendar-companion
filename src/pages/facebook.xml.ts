dfc15218c65c5b8ee38cdf0de0993ac2452f0df1
Elene Tabidze <e.tabidze@myauto.ge>
2024-06-19 14:41:15 +0400

    acebook.xml debug

--- src/pages/facebook.xml.ts
+++ src/pages/facebook.xml.ts
@@ -48,52 +48,75 @@ function FacebookXml() {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res, locale }) => {
-  locale = locale === 'default' || locale === undefined ? 'ka' : locale
+  try {
+    console.log("API_URL:", API_URL);
+    console.log("DOMAIN:", DOMAIN);
 
-  const query = 'page=1&sort_by=id&order_by=desc'
+    locale = locale === 'default' || locale === undefined ? 'ka' : locale
 
-  const paginationResponse = await fetch(`${API_URL}/search-products?${query}`, {
-    headers: {
-      'X-Website-Key': 'myhome',
-      locale
+    const query = 'page=1&sort_by=id&order_by=desc'
+
+    const paginationResponse = await fetch(`${API_URL}/search-products?${query}`, {
+      headers: {
+        'X-Website-Key': 'myhome',
+        locale
+      }
+    })
+
+    if (!paginationResponse.ok) {
+      throw new Error(`Failed to fetch pagination data: ${paginationResponse.statusText}`)
     }
-  })
 
-  const paginationData = await paginationResponse.json()
-  const totalStatements = paginationData.result.total
-  const numberOfRequests = Math.trunc(totalStatements / 50) + 1
+    const paginationData = await paginationResponse.json()
+    const totalStatements = paginationData.result.total
+    const numberOfRequests = Math.trunc(totalStatements / 50) + 1
 
-  const promises: any = []
+    const promises: any = []
 
-  for (let i = 1; i <= numberOfRequests; i++) {
-    promises.push(
-      fetch(`${API_URL}/search-products?${query}&page=${i}`, {
-        headers: {
-          'X-Website-Key': 'myhome',
-          locale
-        }
-      }).then(res => {
-        if (res.ok) {
-          return res.json()
-        }
-      })
-    )
-  }
+    for (let i = 1; i <= numberOfRequests; i++) {
+      promises.push(
+        fetch(`${API_URL}/search-products?${query}&page=${i}`, {
+          headers: {
+            'X-Website-Key': 'myhome',
+            locale
+          }
+        }).then(res => {
+          if (res.ok) {
+            return res.json()
+          } else {
+            throw new Error(`Failed to fetch data for page ${i}: ${res.statusText}`)
+          }
+        })
+      )
+    }
 
-  const response = (await Promise.all(promises)) as any[]
+    const response = (await Promise.all(promises)) as any[]
 
-  const listings = response.flatMap(data => data.result.data)
+    const listings = response.flatMap(data => data.result.data)
 
-  const xml = generateFacebookXml(listings)
+    if (!Array.isArray(listings)) {
+      throw new Error("Invalid data format: listings is not an array");
+    }
 
-  res.setHeader('Content-Type', 'text/xml')
+    const xml = generateFacebookXml(listings)
 
-  //   we send the XML to the browser
-  res.write(xml)
-  res.end()
+    res.setHeader('Content-Type', 'text/xml')
 
-  return {
-    props: {}
+    //   we send the XML to the browser
+    res.write(xml)
+    res.end()
+
+    return {
+      props: {}
+    }
+  } catch (error: any) {
+    console.error('Error generating XML:', error.message, error.stack)
+    res.statusCode = 500
+    res.end('Internal Server Error')
+    
+    return {
+      props: {}
+    }
   }
 }
 
