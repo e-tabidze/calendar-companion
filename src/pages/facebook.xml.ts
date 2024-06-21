80df8f30893d2583668845efad926878102447d0
Elene Tabidze <e.tabidze@myauto.ge>
2024-06-21 15:11:02 +0400

    Fix

--- src/pages/facebook.xml.ts
+++ src/pages/facebook.xml.ts
@@ -48,7 +48,7 @@ function FacebookXml() {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res, locale }) => {
-  try {
+//   try {
     console.log("API_URL:", API_URL);
     console.log("DOMAIN:", DOMAIN);
 
@@ -109,15 +109,15 @@ export const getServerSideProps: GetServerSideProps = async ({ res, locale }) =>
     return {
       props: {}
     }
-  } catch (error: any) {
-    console.error('Error generating XML:', error.message, error.stack)
-    res.statusCode = 500
-    res.end('Internal Server Error'+error.message+"   "+error.stack+"     "+API_URL+"    "+DOMAIN)
+//   } catch (error: any) {
+//     console.error('Error generating XML:', error.message, error.stack)
+//     res.statusCode = 500
+//     res.end('Internal Server Error'+error.message+"   "+error.stack+"     "+API_URL+"    "+DOMAIN)
     
-    return {
-      props: {}
-    }
-  }
+//     return {
+//       props: {}
+//     }
+//   }
 }
 
 export default FacebookXml
