25fa80b439606516ae76e0b9128cce87fc7221a0
etabidze <etabidze@outlook.com>
2024-08-11 01:08:38 +0400

    remove redirect

--- src/utils/handleUserRedirection.ts
+++ src/utils/handleUserRedirection.ts
@@ -34,8 +34,10 @@ export const handleUserRedirection = (userData: any, router: any) => {
   if (userData) {
     console.log(userData, 'userData')
     if (userData?.username_verified_at === null) {
+
       // if (!!userData?.information?.nickname === false) {
         router.push('/getting-started')
+        
       // } else if (userData.active_profile === null) {
       //   router.push('/workspace')
       // } else if (userData.account_connection.length === 0 || userData.active_profile.calendars.length === 0) {
