f2d89a0bee3b4b8e77edbf7978c0262ce578c949
etabidze <etabidze@outlook.com>
2024-08-11 01:07:10 +0400

    remove redirect

--- src/utils/handleUserRedirection.ts
+++ src/utils/handleUserRedirection.ts
@@ -34,16 +34,15 @@ export const handleUserRedirection = (userData: any, router: any) => {
   if (userData) {
     console.log(userData, 'userData')
     if (userData?.username_verified_at === null) {
-      console.log(!!userData?.information?.nickname, '!!userData?.information?.nickname')
-      if (!!userData?.information?.nickname === false) {
+      // if (!!userData?.information?.nickname === false) {
         router.push('/getting-started')
-      } else if (userData.active_profile === null) {
-        router.push('/workspace')
-      } else if (userData.account_connection.length === 0 || userData.active_profile.calendars.length === 0) {
-        router.push('/connect-account')
-      } else {
-        router.push('/calendar')
-      }
+      // } else if (userData.active_profile === null) {
+      //   router.push('/workspace')
+      // } else if (userData.account_connection.length === 0 || userData.active_profile.calendars.length === 0) {
+      //   router.push('/connect-account')
+      // } else {
+      //   router.push('/calendar')
+      // }
     } else {
       router.push('/calendar')
     }
