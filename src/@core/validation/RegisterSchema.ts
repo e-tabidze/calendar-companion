e34fe278a014612735f7d4a83ede427b35edcb46
etabidze <etabidze@outlook.com>
2024-08-29 14:16:35 +0400

    COM-24

--- src/@core/validation/RegisterSchema.ts
+++ src/@core/validation/RegisterSchema.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup'
 
 const RegisterSchema = Yup.object().shape({
-  username: Yup.string().email('email_format_error').nullable().required('Email is required'),
+  username: Yup.string().email('Email format error').nullable().required('Email is required'),
   password: Yup.string()
     .required('Password is required')
     .min(8, 'Password must be at least 8 characters')
@@ -11,7 +11,7 @@ const RegisterSchema = Yup.object().shape({
     .matches(/[0-9]/, 'Password must contain at least one number')
     .matches(/[\W_]/, 'Password must contain at least one special character'),
   repeat_password: Yup.string()
-    .oneOf([Yup.ref('password')], 'passwords_must_match')
+    .oneOf([Yup.ref('password')], 'Password must match')
     .required('Password is required'),
   terms_and_conditions: Yup.string().oneOf(['1'], 'Terms and conditions must be accepted')
 })
