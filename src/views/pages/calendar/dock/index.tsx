c4d9ca00b3605edbf80f8867bb11a32787439e19
etabidze <etabidze@outlook.com>
2024-09-06 15:06:58 +0400

    dock styling fix

--- src/views/pages/calendar/dock/index.tsx
+++ src/views/pages/calendar/dock/index.tsx
@@ -12,11 +12,11 @@ const Dock = () => {
 
   return (
     <div className='w-1/2 mx-auto'>
-      <div className='fixed bottom-0 w-1/2 bg-white shadow-md flex justify-around items-center h-16 z-50 rounded-xl'>
+      <div className='fixed bottom-6 w-1/2 bg-white shadow-md flex justify-around items-center h-16 z-50 rounded-xl'>
         {dockItems.map((item, index) => (
-          <button key={index} className='flex flex-col items-center text-gray-600 hover:text-orange-500'>
-            <Icon svgPath={item.icon} width={24} height={24} />
-            <span className='text-xs'>{item.label}</span>
+          <button key={index} className='flex flex-col items-center text-gray-600 hover:text-raisin-110 group'>
+            <Icon svgPath={item.icon} width={24} height={24} color="#A2A2A2" className='group-hover:fill-[#2E2E2E] transition-all' />
+            <span className='text-xs mt-1'>{item.label}</span>
           </button>
         ))}
       </div>
