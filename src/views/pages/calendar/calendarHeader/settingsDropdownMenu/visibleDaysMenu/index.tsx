35caa43467c78e5ead7e2203e276512988f25e92
etabidze <etabidze@outlook.com>
2024-09-13 16:18:23 +0400

    styling on open visible days

--- src/views/pages/calendar/calendarHeader/settingsDropdownMenu/visibleDaysMenu/index.tsx
+++ src/views/pages/calendar/calendarHeader/settingsDropdownMenu/visibleDaysMenu/index.tsx
@@ -39,28 +39,40 @@ const VisibleDaysMenu = () => {
   return (
     <div className='text-left relative'>
       <Menu>
-        <MenuButton className='inline-flex items-center hover:bg-primary-10 w-full rounded gap-2 py-1.5 px-3 text-raisin-80 font-medium focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white'>
-          Visible days
-        </MenuButton>
+        {({ open }) => (
+          <>
+            <MenuButton
+              className={`inline-flex items-center hover:bg-primary-10 w-full rounded gap-2 py-1.5 px-3  font-medium focus:outline-none data-[focus]:outline-1 data-[focus]:outline-white transition ${
+                open ? 'bg-primary-15 text-primary-100' : 'text-raisin-80'
+              }`}
+            >
+              Visible days
+            </MenuButton>
 
-        <MenuItems
-          transition
-          className='absolute right-[230px] w-52 !bg-white shadow-lg origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0'
-        >
-          {visibleDaysMenuItems.map(menuItem => (
-            <MenuItem key={menuItem?.value}>
-              <button
-                className={`group flex w-full items-center justify-between gap-2 text-raisin-90 font-medium rounded py-1.5 px-3 hover:bg-primary-10 ${
-                  visibleDays === menuItem.value ? 'bg-primary-10 pointer-events-none' : ''
-                }`}
-                onClick={() => setVisibleDays(menuItem.value)}
-              >
-                <span>{menuItem?.label}</span>
-                {visibleDays == menuItem?.value ? <Icon svgPath='tick' width={16} height={16} /> : <span>{menuItem?.value}</span>}
-              </button>
-            </MenuItem>
-          ))}
-        </MenuItems>
+            <MenuItems
+              transition
+              className='absolute right-[230px] w-52 !bg-white shadow-lg origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0'
+            >
+              {visibleDaysMenuItems.map(menuItem => (
+                <MenuItem key={menuItem?.value}>
+                  <button
+                    className={`group flex w-full items-center justify-between gap-2 text-raisin-90 font-medium rounded py-1.5 px-3 hover:bg-primary-10 ${
+                      visibleDays === menuItem.value ? 'bg-primary-10 pointer-events-none' : ''
+                    }`}
+                    onClick={() => setVisibleDays(menuItem.value)}
+                  >
+                    <span>{menuItem?.label}</span>
+                    {visibleDays === menuItem?.value ? (
+                      <Icon svgPath='tick' width={16} height={16} />
+                    ) : (
+                      <span>{menuItem?.value}</span>
+                    )}
+                  </button>
+                </MenuItem>
+              ))}
+            </MenuItems>
+          </>
+        )}
       </Menu>
     </div>
   )
