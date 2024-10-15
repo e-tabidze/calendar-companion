b6f842933fa046494b9d8fc83afa8a8846e37e5e
etabidze <etabidze@outlook.com>
2024-10-15 21:34:01 +0400

    removed calendars length

--- src/utils/handleUserRedirection.ts
+++ src/utils/handleUserRedirection.ts
@@ -37,7 +37,7 @@ export const handleUserRedirection = (userData: any, router: any) => {
         router.push('/getting-started')
       } else if (userData.active_profile === null) {
         router.push('/workspace')
-      } else if (userData.account_connection.length === 0 || userData.active_profile.calendars.length === 0) {
+      } else if (userData.account_connection.length === 0) {
         router.push('/connect-account')
       } else {
         router.push('/calendar')
