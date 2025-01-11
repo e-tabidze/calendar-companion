53a6e8deb49e4b474c9cd26fce17d499597fc75d
etabidze <etabidze@outlook.com>
2025-01-11 17:40:04 +0400

    starting agenda

--- src/pages/details/[[...slug]].tsx
+++ src/pages/details/[[...slug]].tsx
@@ -28,8 +28,7 @@ const Details = () => {
       <div className='max-w-6xl mx-auto'>
         <EventOverview eventDetails={eventDetails} />
         <EventDetails eventDetails={eventDetails} meetingJson={meetingJson} />
-        <MeetingNotes transcript={meetingJson?.transcript} mp4={"https://s3.eu-west-3.amazonaws.com/meeting-baas-video/7fe13b74-3cb0-444c-a440-5fd021c6d795.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARHQBNK4AXVQ77Y37%2F20241231%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20241231T093937Z&X-Amz-Expires=3600&X-Amz-Signature=0e3d6c52683f13c42f11b029b46946345df29e59ee1aed2191e80969491e4513&X-Amz-SignedHeaders=host"} eventDetails={eventDetails} />
-
+        <MeetingNotes transcript={meetingJson?.transcript} mp4={meetingJson?.mp4} eventDetails={eventDetails} />
       </div>
     </div>
   )
--- src/views/pages/details/eventDetails/index.tsx
+++ src/views/pages/details/eventDetails/index.tsx
@@ -8,39 +8,35 @@ interface EventDetailsProps {
 }
 
 const EventDetails: React.FC<EventDetailsProps> = ({ eventDetails }) => {
-
   return (
-    <div>
-      
-      <div className='max-w-6xl mx-auto'>
-        <div className='relative pl-8 border-l-2 border-gray-100'>
-          <div className='relative'>
-            <Icon svgPath='action2' width={20} height={20} className='absolute -left-[2.2rem]' />
+    <div className='flex gap-4 mt-16'>
+      <div className='relative z-10'>
+        <div className='absolute h-full w-px bg-raisin-10 left-[9px] -top-4 z-10' />
+        <Icon svgPath='action2' width={18} height={18} className='hidden lg:inline-block z-20 relative' />
+      </div>
+      <div className=''>
+        <div className='flex items-center gap-3 mb-2'>
+          <img src='/api/placeholder/32/32' className='w-8 h-8 rounded-full' />
+          <div>
+            <span className='font-medium'>{eventDetails.event_data.creator.email}</span>
+            <span className='text-gray-600 ml-2'>Edited an agenda</span>
+            <span className='text-gray-500 ml-2'>21 aug · 16:33am</span>
+          </div>
+        </div>
 
-            <div className='flex items-center gap-3 mb-2'>
-              <img src='/api/placeholder/32/32' className='w-8 h-8 rounded-full' />
-              <div>
-                <span className='font-medium'>{eventDetails.event_data.creator.email}</span>
-                <span className='text-gray-600 ml-2'>Edited an agenda</span>
-                <span className='text-gray-500 ml-2'>21 aug · 16:33am</span>
-              </div>
-            </div>
-
-            <div className='mt-4'>
-              <div className='flex items-center justify-between mb-4'>
-                <h3 className='text-xl font-semibold'>Meeting agenda</h3>
-                <button>
-                  <MoreHorizontal className='w-5 h-5 text-gray-400' />
-                </button>
-              </div>
-              <div contentEditable>
-              <div className='text-[#FF5A1F]'>📍 It consists of 15 items and 33 sub-items</div>
-              <p className='mt-4 text-gray-600' >
-                {eventDetails.event_data.description ||
-                  'Suspendisse quis erat non ligula sollicitudin pulvinar ac ac velit. Fusce quam enim, tristique vel sem vitae, finibus pulvinar velit.'}
-              </p>
-            </div>
-            </div>
+        <div className='mt-4'>
+          <div className='flex items-center justify-between mb-4'>
+            <h3 className='text-xl font-semibold'>Meeting agenda</h3>
+            <button>
+              <MoreHorizontal className='w-5 h-5 text-gray-400' />
+            </button>
+          </div>
+          <div contentEditable>
+            <div className='text-[#FF5A1F]'>📍 It consists of 15 items and 33 sub-items</div>
+            <p className='mt-4 text-gray-600'>
+              {eventDetails.event_data.description ||
+                'Suspendisse quis erat non ligula sollicitudin pulvinar ac ac velit. Fusce quam enim, tristique vel sem vitae, finibus pulvinar velit.'}
+            </p>
           </div>
         </div>
       </div>
